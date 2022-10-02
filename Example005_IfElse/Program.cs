b5f3f599fe1ed2d65a8841e41b0dc4856f2ef12e
Renat Prokopiev <prokop4891@gmail.com>
2022-10-02 19:11:23 +0300

    Ex005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter Username: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("How you doing, Masha!");
+}    
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
